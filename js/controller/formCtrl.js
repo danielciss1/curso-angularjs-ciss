48c00f3942da25a5fd1ce8a7df487732d289d830
danielciss1 <danieldelajustina@ciss.com.br>
2016-03-15 07:58:07 -0300

    upload

---
+++ js/controller/formCtrl.js
@@ -0,0 +1,9 @@
+myApp.controller('FormCtrl',function($scope){
+	$scope.pessoa = {
+		name : 'Daniel',
+		lastname : 'Teste',
+		age: 28,
+		fone: '46-9114-7640',
+		email: 'dd.justina@gmail.com'
+	}
+});
